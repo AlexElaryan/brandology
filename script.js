462c0714b5af9b0fc5a227c91f0166e367b22949
AlexElaryan <alexelaryan1@gmail.com>
2025-02-06 22:07:07 +0400

    deploy

--- script.js
+++ script.js
@@ -18,3 +18,18 @@ circlesExp.forEach(circle => {
         circlesExp.forEach(c => c.classList.remove('small-exp'));
     });
 });
+
+
+const workPtitle = document.querySelectorAll('.work-process_item-title');
+const workPitem = document.querySelectorAll('.work-process_item');
+
+workPtitle.forEach((el, ind) => {
+    el.onclick = () => {
+        if (!workPitem[ind].classList.contains('work-process_item-active')) {
+            workPitem[ind].classList.add('work-process_item-active');
+        } else {
+            workPitem[ind].classList.remove('work-process_item-active');
+        }
+        
+    }
+})
