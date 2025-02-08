bb23674091fc059098d02a5d7226e746f3d50d95
AlexElaryan <alexelaryan1@gmail.com>
2025-02-08 19:45:40 +0400

    deploy

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+const circles = document.querySelectorAll('.mission-circle');
+const circlesExp = document.querySelectorAll('.expert-circle');
+const circlesRef = document.querySelectorAll('.refauna_circles-item');
+
+function forCircleHover(circle, classN) {
+    if (circle) {
+        circle.forEach(el => {
+            el.addEventListener('mouseenter', () => {
+                circle.forEach(c => c !== el && c.classList.add(classN));
+            });
+            el.addEventListener('mouseleave', () => {
+                circle.forEach(c => c.classList.remove(classN));
+            });
+        })
+    }
+}
+
+forCircleHover(circles, 'small');
+forCircleHover(circlesExp, 'small-exp');
+forCircleHover(circlesRef, 'small-ref');
+
+const toCases = document.querySelector('.to_cases');
+const toCasesBlock = document.querySelector('.intro-to-cases-block');
+const intro = document.querySelector('.intro');
+
+toCases.onclick = () => {
+    toCasesBlock.style.transform = 'scale(1)';
+    intro.style.transform = 'scale(0)';
+    document.body.style.overflow = 'hidden';
+    document.documentElement.scrollTop = 0;
+}
