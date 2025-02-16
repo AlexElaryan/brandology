8e6ca900cd8ec443560c1c4d9d418fb1fe7553b7
AlexElaryan <alexelaryan1@gmail.com>
2025-02-16 23:38:32 +0400

    dep

--- scripts/script.js
+++ scripts/script.js
@@ -18,15 +18,3 @@ function forCircleHover(circle, classN) {
 forCircleHover(circles, 'small');
 forCircleHover(circlesExp, 'small-exp');
 forCircleHover(circlesRef, 'small-ref');
-
-const toCases = document.querySelector('.to_cases');
-const toCasesBlock = document.querySelector('.intro-to-cases-block');
-const intro = document.querySelector('.intro');
-
-toCases.onclick = () => {
-    toCasesBlock.style.transform = 'scale(1)';
-    intro.style.transform = 'scale(0)';
-
-    document.body.style.overflow = 'hidden';
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-}
